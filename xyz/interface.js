f27b10fa53bb11215ea293675b7c5121cd2a9f70
Dev <dchaud26@uwo.ca>
2024-05-05 07:06:20 -0400

    go

---
+++ xyz/interface.js
@@ -0,0 +1,11 @@
+const accountForm = document.getElementById('account-form');
+const addFriendForm = document.getElementById('add-friend-form');
+const sendMessageForm = document.getElementById('send-message-form');
+const nameInput = document.getElementById('name');
+const friendAddressInput = document.getElementById('friend-address');
+const friendNameInput = document.getElementById('friend-name');
+const messageContentInput = document.getElementById('message-content');
+const messagesList = document.getElementById('messages-list');
+const friendsList = document.getElementById('friends-list');
+
+// <!-- // Connect to the blockchain and replace with your ABI and contract address -->
